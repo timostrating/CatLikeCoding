637d49df9e5398181b074d00bcab50c358b291b3
Timo Strating <timostrating@home.nl>
2017-10-18 22:57:40 +0200

    started on the tutorial on Object Pools

---
+++ CatlikeCoding-ObjectPools/Assets/Stuff.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Stuff : MonoBehaviour {
+
+	Rigidbody body;
+
+	void Awake () {
+		body = GetComponent<Rigidbody>();
+	}
+}
---
+++ CatlikeCoding-ObjectPools/Assets/StuffSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuffSpawner : MonoBehaviour {
+
+	[SerializeField]private float timeBetweenSpawns;
+    [SerializeField]private Stuff[] stuffPrefabs;
+
+    float timeSinceSpawn;
+
+    void FixedUpdate () {
+        timeSinceSpawn += Time.deltaTime;
+        if (timeSinceSpawn >= timeBetweenSpawns) {
+			timeBetweenSpawns -= timeBetweenSpawns;
+            SpawnStuff ();
+        }
+    }
+
+    void SpawnStuff () {
+        Stuff spawn = Instantiate<Stuff> (stuffPrefabs[Random.Range (0, stuffPrefabs.Length)]);
+        spawn.transform.localPosition = transform.position;
+    }
+}
