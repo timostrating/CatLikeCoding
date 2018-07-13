f58d2b4b1510cee5ac8b741d34f4a1b0e9c1ada6
timostrating <timostrating@home.nl>
2018-07-14 00:31:12 +0200

    started on the HexMap serie

---
+++ CatlikeCoding-HexMap01/Assets/Code/HexGrid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+
+	public int width = 6;
+	public int height = 6;
+
+	public HexCell cellPrefab;
+	HexCell[] cells;
+
+	public Text cellLabelPrefab;
+	Canvas gridCanvas;
+
+
+	void Awake() {
+		gridCanvas = GetComponentInChildren<Canvas>();
+
+		cells = new HexCell[height * width];
+
+		for (int z=0, i=0; z < height; z++) {
+			for (int x=0; x < width; x++) {
+				CreateCell(x, z, i++);
+			}
+		}
+	}
+
+	void CreateCell(int x, int z, int i) {
+		Vector3 pos = new Vector3(x * 10f, 0, z * 10f);
+
+		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = pos;
+	}
+
+}
---
+++ CatlikeCoding-HexMap01/Assets/Code/HexMetrics.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class HexMetrics : MonoBehaviour {
+
+	
+	public const float outerRadius = 10f;
+
+	public const float innerRadius = outerRadius * 0.866025404f; 	// outer * sqrt(3) / 2
+
+
+	public static Vector3[] corners = {
+		new Vector3(0, 0, outerRadius),
+		new Vector3(innerRadius, 0, 0.5f * outerRadius), 		// Mathf.Sqrt(Mathf.Pow(outerRadius, 2) - Mathf.Pow(innerRadius, 2))),
+		new Vector3(innerRadius, 0, -0.5f * outerRadius), 		// Mathf.Sqrt(Mathf.Pow(outerRadius, 2) - Mathf.Pow(innerRadius, 2))),
+		new Vector3(0, 0, -outerRadius),
+		new Vector3(-innerRadius, 0, -0.5f * outerRadius), 		// Mathf.Sqrt(Mathf.Pow(outerRadius, 2) - Mathf.Pow(innerRadius, 2))),
+		new Vector3(-innerRadius, 0, 0.5f * outerRadius), 		// Mathf.Sqrt(Mathf.Pow(outerRadius, 2) - Mathf.Pow(innerRadius, 2)))
+	};
+
+}
